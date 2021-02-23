f9f0308abefc27945b00f3b616b6d254cb163ef6
devadilson <didasoueudida@gmail.com>
2021-02-23 18:22:53 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:splash/splash.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      home: Splash(),
+    );
+  }
+}
