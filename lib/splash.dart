f3591c46dcf7ecfd2b752841b1fc6bc0d4303fc2
devadilson <didasoueudida@gmail.com>
2021-02-23 18:51:42 -0300

    Logo

--- lib/splash.dart
+++ lib/splash.dart
@@ -4,11 +4,29 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Image.asset('assets/images/especializati.png'),
-          CircularProgressIndicator()
-        ],
+      body: Container(
+        width: double.maxFinite,
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 250,
+              child:
+                  Image.asset('assets/images/logo-especializati-branca2.png'),
+            ),
+            Container(height: 40),
+            CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+            ),
+            Container(height: 20),
+            Text(
+              'Carregando...',
+              style: TextStyle(color: Colors.white, fontSize: 16.0),
+            ),
+          ],
+        ),
       ),
     );
   }
